9b68e1d0b87b79c23c633ec3dad495c048e8bfa6
jamesmshihua <64199215+jamesmshihua@users.noreply.github.com>
2024-12-11 16:17:03 +0800

    Initial Commit

---
+++ PlotDigitizer/PlotDigitizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotDigitizerApp.swift
+//  PlotDigitizer
+//
+//  Created by James Shihua on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlotDigitizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlotDigitizerTests/PlotDigitizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlotDigitizerTests.swift
+//  PlotDigitizerTests
+//
+//  Created by James Shihua on 11/12/2024.
+//
+
+import Testing
+@testable import PlotDigitizer
+
+struct PlotDigitizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
